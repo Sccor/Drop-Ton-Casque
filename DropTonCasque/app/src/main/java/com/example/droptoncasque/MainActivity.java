d59b30266e541bbd312ed2ea69c65dbe8b3e7d78
Sccor <stan.meline@gmail.com>
2020-11-02 09:31:39 +0100

    login activity

--- DropTonCasque/app/src/main/java/com/example/droptoncasque/MainActivity.java
+++ DropTonCasque/app/src/main/java/com/example/droptoncasque/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.droptoncasque;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.log = (ImageView) findViewById(R.id.log);
+
+        log.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginActivity);
+                finish();
+            }
+        });
     }
 }
