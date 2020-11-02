29707b36b77bbace5f6e531ccf100fe1471b2336
Sccor <stan.meline@gmail.com>
2020-11-02 12:21:29 +0100

    menu bouton dans map et login activity

--- DropTonCasque/app/src/main/java/com/example/droptoncasque/MapsActivity.java
+++ DropTonCasque/app/src/main/java/com/example/droptoncasque/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.droptoncasque;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private Button menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        this.menu = (Button) findViewById(R.id.map_button_menu);
+        menu.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+                finish();
+            }
+        });
+
     }
 
     /**
@@ -37,9 +51,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        LatLng efrei = new LatLng(48.78874841816021, 2.3637836840078177);
-        mMap.addMarker(new MarkerOptions().position(efrei).title("Marker in Efrei"));
+        LatLng efrei = new LatLng(48.788759834312756, 2.363766951205992);
+        mMap.addMarker(new MarkerOptions().position(efrei).title("Marker in Efrei Paris"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(efrei));
         float zoomLevel = 16.0f;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(efrei, zoomLevel));
