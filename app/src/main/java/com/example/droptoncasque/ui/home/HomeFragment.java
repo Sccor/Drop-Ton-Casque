b4087abf9bd8b31ec34e88d695f8227922b23a8d
Sccor <stan.meline@gmail.com>
2021-01-09 20:16:49 +0100

    changement du titre

--- app/src/main/java/com/example/droptoncasque/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/droptoncasque/ui/home/HomeFragment.java
@@ -33,13 +33,6 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.textView2);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         NavigationView navigationView = (NavigationView) getActivity().findViewById(R.id.nav_view);
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
         if (sharedPref.contains("User_Role")){
